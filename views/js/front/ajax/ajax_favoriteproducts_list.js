b8927b12aee9fc86c1aa02dcfd7d6079be3ff726
Roman Matviy <admin@matviy.pp.ua>
2020-09-23 13:41:15 +0300

    Click and rename btn checkbox all to uncheckbox all

--- views/js/front/ajax/ajax_favoriteproducts_list.js
+++ views/js/front/ajax/ajax_favoriteproducts_list.js
@@ -1,20 +1,42 @@
 console.log('ajax_favoriteproducts_list.js');
 
-$("button").click(function () {
-    $("p").hide();
+$('article .addstar').click(function () {
+    $(this).hide();
+
+    var str = $(this).val();
+    console.log('ss' + str);
 });
 
 
 
 $(document).on('click', '#checkbox_all', function () {
     $('.check').not(this).prop('checked', this.checked);
+    const $this = $('.checkall');
+    $this.toggleClass('changetext');
+    if ($this.hasClass('changetext')) {
+        $this.text('UNCHECKBOX ALL');
+    } else {
+        $this.text('CHECKBOX ALL');
+    }
 });
 
 
 $(document).on('change', '.check', function () {
     if ($('.check:checked').length == $('.check').length) {
         $('#checkbox_all').prop('checked', true);
+        const $this = $('.checkall');
+        $this.toggleClass('changetext');
+        if (!$this.hasClass('changetext')) {
+            $this.text('CHECKBOX ALL');
+        } else {
+            $this.text('UNCHECKBOX ALL');
+        }
     } else {
         $('#checkbox_all').prop('checked', false);
+        const $this = $('.checkall');
+        $this.toggleClass('changetext');
+        if (!$this.hasClass('changetext')) {
+            $this.text('CHECKBOX ALL');
+        }
     };
 });
